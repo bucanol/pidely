37721e9386566cfef1b542a934b3521ecddb6991
bucanol <torrrsalejandro173@gmail.com>
2026-03-18 19:00:07 -0600

    fix: deploy production tweaks

--- server/db.ts
+++ server/db.ts
@@ -2,11 +2,10 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "@shared/schema";
 
-// Configuramos el Pool para que acepte SSL de Neon
 export const pool = new Pool({ 
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // Esto permite la conexión segura con Neon
+    rejectUnauthorized: false // <--- Esto corrige el error 500 en Render
   }
 });
 
