3d39637e6bd9bf757e9c3bef427e96c498c6c9a6
bucanol <torrrsalejandro173@gmail.com>
2026-03-18 18:51:58 -0600

    fix: pool connection with ssl

--- server/db.ts
+++ server/db.ts
@@ -2,5 +2,12 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "@shared/schema";
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+// Configuramos el Pool para que acepte SSL de Neon
+export const pool = new Pool({ 
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Esto permite la conexión segura con Neon
+  }
+});
+
 export const db = drizzle(pool, { schema });
