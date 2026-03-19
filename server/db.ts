67ba5cbc8a81afaa458d310bf6efb64cb73d9445
bucanol <torrrsalejandro173@gmail.com>
2026-03-18 19:04:11 -0600

    fix: server index y db ssl

--- server/db.ts
+++ server/db.ts
@@ -5,7 +5,7 @@ import * as schema from "@shared/schema";
 export const pool = new Pool({ 
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // <--- Esto corrige el error 500 en Render
+    rejectUnauthorized: false 
   }
 });
 
